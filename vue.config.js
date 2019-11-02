f2b76034bd276d9f9fccb186a804a1856843f5f9
Franck <abgrallkefran@gmail.com>
2019-11-02 18:55:36 +0100

    Trigger CI

--- vue.config.js
+++ vue.config.js
@@ -8,7 +8,7 @@ const merge = require('webpack-merge')
 const defaultConfiguration = require('./vue-config/config.default')
 
 const environmentConfigurationPath =
-  './vue-config/config.' + process.env.NODE_ENV + '.js'
+  `./vue-config/config.${  process.env.NODE_ENV  }.js`
 
 const environmentConfiguration = fs.existsSync(environmentConfigurationPath)
   ? require(environmentConfigurationPath) // eslint-disable-line
